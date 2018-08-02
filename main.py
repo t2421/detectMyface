41a359050adaab1d3278368c3464a3a435b054b7
Taiga Kiyotaki <t-kiyotaki@bfp.co.jp>
2018-08-02 22:12:31 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import cv2, os
+import cv2, os, json
 import numpy as np
 from PIL import Image
 
-print(cv2.__version__)
 train_path = './train'
 test_path = './test'
 
-#顔を認識する
+#Haar-like特徴分類器で顔を認識するための準備
 cascadePath = "./haarcascade_frontalface_alt.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 
@@ -16,7 +15,7 @@ recognizer = cv2.face.EigenFaceRecognizer_create()
 
 # labelをintで管理するため
 human_labels = {}
-
+result = {}
 # 指定されたpath内の画像を取得
 def get_images_and_labels(path):
     count = 0
@@ -40,14 +39,12 @@ def get_images_and_labels(path):
             return
         # Haar-like特徴分類器で顔を検知
         faces = faceCascade.detectMultiScale(image)
+
         # 検出した顔画像の処理
         for (x, y, w, h) in faces:
-            # 顔を 200x200 サイズにリサイズ
+            # 検出した顔の部分をクリップして 200x200 サイズにリサイズ
             roi = cv2.resize(image[y: y + h, x: x + w], (200, 200), interpolation=cv2.INTER_LINEAR)
-        #     # 画像を配列に格納
             images.append(roi)
-            # cv2.imwrite("./"+"clip_"+f, roi)
-        #     # ファイル名からラベルを取得
 
             # 解析で渡せるラベルはintだけなので、それぞれの人をintに割り当てる
             use_label = get_label(f)
@@ -56,9 +53,8 @@ def get_images_and_labels(path):
                 count = count+1
             labels.append(human_labels[use_label])
             
-        #     # ファイル名を配列に格納
+            # ファイル名を配列に格納
             files.append(f)
-    
     return images, labels, files
 
 def get_label(filename):
@@ -67,27 +63,32 @@ def get_label(filename):
 
 # # トレーニング画像を取得
 images, labels, files = get_images_and_labels(train_path)
-
-# # トレーニング実施
 recognizer.train(images, np.array(labels))
 
 # # テスト画像を取得
 test_images, test_labels, test_files = get_images_and_labels(test_path)
 
 i = 0
+
+
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
+
+
 while i < len(test_labels):
     # テスト画像に対して予測実施
-    
     label, confidence = recognizer.predict(test_images[i])
     
     # 予測結果をコンソール出力
     print("Test Image: {}, Predicted Label: {}, Confidence: {}".format(test_files[i], label, confidence))
     # テスト画像を表示
-    cv2.imshow("test image", test_images[i])
-    
-    cv2.waitKey(300)
-
+    cv2.imshow("test image", test_images[i]) 
+    result["label"] = label
+    result["confidence"] = confidence
+    result["label_str"] = get_keys_from_value(human_labels,label)[0]
+    print(json.dumps(result))
     i += 1
+
 
 # 終了処理
 cv2.destroyAllWindows()
